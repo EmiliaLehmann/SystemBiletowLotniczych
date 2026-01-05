11ce7f6c3b3b0cf2ad17cd496ed2874b1bde1c9b
Emilia Lehmann <emilka.lehmann06@gmail.com>
2026-01-05 13:19:30 +0100

    dodanie exception

--- SystemBiletowLotniczych/Bilet.cs
+++ SystemBiletowLotniczych/Bilet.cs
@@ -7,22 +7,28 @@ namespace SystemBiletowLotniczych
     public abstract class Bilet
     {
         private double cena;
-        public string NumerBiletu;
-        public string ImiePasazera;
-        public string NazwiskoPasazera;
+        private string numerBiletu;
+        private string imiePasazera;
+        private string nazwiskoPasazera;
+        private DateTime DataWylotu;
+        private TimeOnly godzinaWylotu;
+
+
         public double Cena
         {
             get { return cena; }
             set
             {
                 if (value < 0)
-                    throw new ArgumentException("Cena nie moze byc ujemna.");
+                    throw new ZlaCenaException("Cena nie moze byc ujemna.");
                 cena = value;
             }
         }
 
-
-
-
+        public string NumerBiletu { get => numerBiletu; set => numerBiletu = value; }
+        public string ImiePasazera { get => imiePasazera; set => imiePasazera = value; }
+        public string NazwiskoPasazera { get => nazwiskoPasazera; set => nazwiskoPasazera = value; }
+        public DateTime DataWylotu1 { get => DataWylotu; set => DataWylotu = value; }
+        public TimeOnly GodzinaWylotu { get => godzinaWylotu; set => godzinaWylotu = value; }
     }
 }
---
+++ SystemBiletowLotniczych/ZlaCenaException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SystemBiletowLotniczych
+{
+    internal class ZlaCenaException : Exception
+    {
+        public ZlaCenaException(string message) : base(message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+}
