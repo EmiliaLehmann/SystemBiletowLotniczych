2b60cccd599f380723db8816e8c9eac4e0730fa8
Emilia Lehmann <emilka.lehmann06@gmail.com>
2026-01-05 14:20:33 +0100

    cos sie dzieje

--- SystemBiletowLotniczych/BiletKrajowy.cs
+++ SystemBiletowLotniczych/BiletKrajowy.cs
@@ -4,9 +4,29 @@ using System.Text;
 
 namespace SystemBiletowLotniczych
 {
-    public class BiletKrajowy: Bilet
+    public class BiletKrajowy : Bilet
     {
+        public double StawkaPodatkowa = 0.08;
 
-    
+        public BiletKrajowy() : base()
+        {
+        }
+
+        public BiletKrajowy(string numerBiletu, string imiePasazera, string nazwiskoPasazera, double cena, DateTime dataWylotu, TimeOnly godzinaWylotu, EnumKlasa klasa, DateTime dataRezerwacji, string miastoWylotu, string miastoPrzylotu)
+            : base(numerBiletu, imiePasazera, nazwiskoPasazera, cena, dataWylotu, godzinaWylotu, klasa, miastoPrzylotu)
+        {
+        }
+        public override double ObliczCeneKoncowa()
+        {
+            double cenabazowa = base.ObliczCeneKoncowa();
+
+            return cenabazowa * (1 + StawkaPodatkowa);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", StawkaPodatkowa: {StawkaPodatkowa:P}";
+
+        }
     }
 }
--- SystemBiletowLotniczych/BiletZPrzesiadkamics.cs
+++ SystemBiletowLotniczych/BiletZPrzesiadkamics.cs
@@ -4,7 +4,10 @@ using System.Text;
 
 namespace SystemBiletowLotniczych
 {
-    internal class BiletZPrzesiadkamics
+    public class BiletZPrzesiadkamics
     {
+        private List<Bilet> bilety;
+
+        public List<Bilet> Bilety { get => bilety; set => bilety = value; }
     }
 }
