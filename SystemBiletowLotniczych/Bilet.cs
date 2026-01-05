892876b11b839edf2f83b0d8c75eafbbfe94faf6
Emilia Lehmann <emilka.lehmann06@gmail.com>
2026-01-05 13:10:06 +0100

    stworzenie klasy Bilet

---
+++ SystemBiletowLotniczych/Bilet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SystemBiletowLotniczych
+{
+    public abstract class Bilet
+    {
+        private double cena;
+        public string NumerBiletu;
+        public string ImiePasazera;
+        public string NazwiskoPasazera;
+        public double Cena
+        {
+            get { return cena; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Cena nie moze byc ujemna.");
+                cena = value;
+            }
+        }
+
+
+
+
+    }
+}
