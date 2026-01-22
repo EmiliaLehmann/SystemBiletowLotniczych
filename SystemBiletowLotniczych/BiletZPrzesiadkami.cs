4f6f8fdd9bc2afd8ec378b3b994b9b756cd1febc
Kosciug9 <weronika.kosciug@gmail.com>
2026-01-22 22:21:54 +0100

    dodanie pliku README

--- SystemBiletowLotniczych/BiletZPrzesiadkami.cs
+++ SystemBiletowLotniczych/BiletZPrzesiadkami.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace SystemBiletowLotniczych
 {
-    /// <summary>
-    /// Reprezentuje bilet z przesiadkami, składający się z wielu etapów podróży,
-    /// dla których obliczana jest łączna cena z uwzględnieniem zniżki.
-    /// </summary>
+    [NotMapped]
     public class BiletZPrzesiadkami : Bilet
     {
         private List<Bilet> EtapyPodrozy;
@@ -18,47 +16,40 @@ namespace SystemBiletowLotniczych
         public string MiastoKoncowe { get => miastoKoncowe; set => miastoKoncowe = value; }
         public double Znizka { get => znizka; set => znizka = value; }
 
-        /// <summary>
-        /// Tworzy nową instancję biletu z przesiadkami
-        /// oraz inicjalizuje listę etapów podróży.
-        /// </summary>
         public BiletZPrzesiadkami() : base()
         {
             EtapyPodrozy = new List<Bilet>();
-        }
+        } 
 
-        /// <summary>
-        /// Oblicza końcową cenę biletu z przesiadkami
-        /// jako sumę cen poszczególnych etapów z uwzględnieniem zniżki.
-        /// </summary>
-        /// <returns>Końcowa cena biletu z przesiadkami.</returns>
-        public new double ObliczCeneKoncowa()
-        {
-            double suma = 0;
-            foreach (var bilet in EtapyPodrozy)
+     
+            public override double ObliczCeneKoncowa()
             {
-                suma += bilet.ObliczCeneKoncowa();
+                double suma = 0;
+                foreach (var bilet in EtapyPodrozy)
+                {
+                    suma += bilet.ObliczCeneKoncowa();
+                }
+
+                return suma * (1 - znizka);
             }
 
-            return suma * (1 - znizka);
-        }
-
-        /// <summary>
-        /// Generuje numer trasy na podstawie numerów lotów
-        /// poszczególnych etapów podróży.
-        /// </summary>
-        /// <returns>Wygenerowany numer trasy.</returns>
-        public string GenerujNumerTrasy()
-        {
-            if (EtapyPodrozy.Count == 0) return "BRAK-LOTOW";
-
-            string numerTrasy = string.Empty;
-            foreach (var bilet in EtapyPodrozy)
+            public string GenerujNumerTrasy()
             {
-                numerTrasy += bilet.NumerLotu.Substring(0, Math.Min(3, bilet.NumerLotu.Length)) + "-";
-            }
+                if (EtapyPodrozy.Count == 0) return "BRAK-LOTOW";
 
-            return $"{numerTrasy}NR-{NumerMiejsca:D3}";
+                string numerTrasy = string.Empty;
+                foreach (var bilet in EtapyPodrozy)
+                {
+                    numerTrasy += bilet.NumerLotu.Substring(0, Math.Min(3, bilet.NumerLotu.Length)) + "-";
+                }
+
+                return $"{numerTrasy}NR-{NumerMiejsca:D3}";
+            }
         }
     }
-}
+
+
+
+
+
+
